a588be671b84d5660747727a91544e0a7bf2a70e
yudai <yudai201729@icloud.com>
2022-07-14 22:05:16 +0900

    ✨feat:カテゴリー検索機能

---
+++ src/repositories/qa/fetcherQusetion.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosResponse } from "axios";
+import router from "next/router";
+import QAResponse from "../../types/api/qa/qaResponse";
+import question from "../../types/domain/qa/question";
+
+
+export const fetcherQusetion = (url: string): Promise<question[]> => {
+    console.log('fetch question')
+    return (
+        axios.get(url)
+        .then((res: AxiosResponse<QAResponse>) => {
+            const { data, status } = res
+            const resBody: question[] = data.data
+            return resBody
+        }
+        ).catch(
+            ({error}) => {
+                router.replace("/500")
+                return error
+            }
+        )
+    )
+};
