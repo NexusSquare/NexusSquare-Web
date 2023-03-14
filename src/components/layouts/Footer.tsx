3033c0d0913094cd0673facfde0f03f5b4a003eb
yudai2929 <yudai201729@icloud.com>
2023-03-14 12:14:06 +0900

    💄fix: footer style

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -7,63 +7,20 @@ export const Footer = (): JSX.Element => {
     const LOGO_URL: string = '/images/logo.jpg'
     const TWITTER_URL: string = 'https://twitter.com/NexusSquare_apu'
     return (
-        <VStack as="footer" bgColor="#FF9037" spacing="0px" w="100%" minH="200px" paddingTop="50px">
-            <Stack w={{ base: '100%', md: '80%' }} direction={{ base: 'column', md: 'row' }}>
-                <Box d="flex" justifyContent="center">
-                    <ChakraNextImage src={LOGO_URL} alt="NexusSquare" width={200} height={50}></ChakraNextImage>
-                </Box>
-                <Spacer />
-                <Stack as="nav" direction={{ base: 'column', md: 'row' }} align="center" spacing="30px">
-                    <Link href="/" passHref>
-                        <Box
-                            as="a"
-                            href="/"
-                            whiteSpace="nowrap"
-                            fontWeight="700"
-                            color="white"
-                            fontSize="large"
-                            _hover={{ textDecoration: 'underline' }}
-                        >
-                            About
-                        </Box>
-                    </Link>
-                    <Link href="/rule" passHref>
-                        <Box
-                            as="a"
-                            href="/rule"
-                            whiteSpace="nowrap"
-                            fontWeight="700"
-                            color="white"
-                            fontSize="large"
-                            _hover={{ textDecoration: 'underline' }}
-                        >
-                            利用規約
-                        </Box>
-                    </Link>
-                    <Link href="/privacy" passHref>
-                        <Box
-                            as="a"
-                            href="/"
-                            whiteSpace="nowrap"
-                            fontWeight="700"
-                            color="white"
-                            fontSize="large"
-                            _hover={{ textDecoration: 'underline' }}
-                        >
-                            プライバシー・ポリシー
-                        </Box>
-                    </Link>
-                </Stack>
-            </Stack>
-            <Spacer />
-            <HStack paddingTop="30px" justifyContent={{ base: 'center', md: 'end' }} w={{ base: '100%', md: '80%' }}>
+        <VStack as="footer" bgColor="#FF9037" spacing="0px" w="100%" minH="200px" paddingTop={8}>
+            <Stack as="nav" direction={{ base: 'column', md: 'row' }} align="center" spacing={4}>
+                <ChakraNextImage src={LOGO_URL} alt="NexusSquare" width={180} height={45}></ChakraNextImage>
+
+                <FooterLink href="/" text="About" />
+                <FooterLink href="/rule" text="利用規約" />
+                <FooterLink href="privacy" text="プライバシーポリシー" />
                 <Link href={TWITTER_URL} passHref>
                     <HStack as="a" target="_blank">
                         <BsTwitter size={30} color="white" />
                     </HStack>
                 </Link>
-            </HStack>
-            <Spacer />
+            </Stack>
+
             <Text
                 align={{ base: 'center', md: 'end' }}
                 w={{ base: '100%', md: '80%' }}
@@ -74,5 +31,28 @@ export const Footer = (): JSX.Element => {
                 &copy; 2022 Nexus Square
             </Text>
         </VStack>
+    )
+}
+
+interface FooterLinkProps {
+    href: string
+    text: string
+}
+
+const FooterLink = ({ href, text }: FooterLinkProps): JSX.Element => {
+    return (
+        <Link href="/privacy" passHref>
+            <Box
+                as="a"
+                href={href}
+                whiteSpace="nowrap"
+                fontWeight="700"
+                color="white"
+                fontSize="large"
+                _hover={{ textDecoration: 'underline' }}
+            >
+                {text}
+            </Box>
+        </Link>
     )
 }
