e143ea832f1f76d2b0a036890ce2653e881d38f9
yudai2929 <yudai201729@icloud.com>
2023-03-17 21:56:26 +0900

    💄fix: footerの削除

--- src/components/organisms/profile/history/QuestionList.tsx
+++ src/components/organisms/profile/history/QuestionList.tsx
@@ -76,13 +76,13 @@ export const QuestionList = ({ questions, isLoading, onClickCard, refetchQuestio
     return (
         <>
             {isLoading || !questions ? (
-                <VStack as="section" w="full" spacing={0} pb="24">
+                <VStack as="section" w="full" spacing={0}>
                     <QASkeleton />
                     <QASkeleton />
                     <QASkeleton />
                 </VStack>
             ) : (
-                <VStack as="section" w="full" spacing={0} pb="24">
+                <VStack as="section" w="full" spacing={0}>
                     {questions.length > 0 ? (
                         <>
                             {questions.map((questions: Question) => {
