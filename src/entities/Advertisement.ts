eac1e3efb3dfba83d18c599db628c6ebb211fc8b
yudai2929 <yudai201729@icloud.com>
2023-03-29 23:16:48 +0900

    👍feat: add advertisement entity

--- src/entities/Advertisement.ts
+++ src/entities/Advertisement.ts
@@ -6,4 +6,21 @@ class Advertisement {
         const random = Math.floor(Math.random() * this.sponsers.length)
         return this.sponsers[random]
     }
+
+    public getAll = (): Sponser[] => {
+        return this.sponsers
+    }
+
+    public getSome = (count: number): Sponser[] => {
+        const randoms: number[] = []
+        const result: Sponser[] = []
+        while (randoms.length < count) {
+            const random = Math.floor(Math.random() * this.sponsers.length)
+            if (!randoms.includes(random)) {
+                randoms.push(random)
+                result.push(this.sponsers[random])
+            }
+        }
+        return result
+    }
 }
