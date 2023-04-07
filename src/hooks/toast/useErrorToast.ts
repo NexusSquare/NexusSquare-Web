48835faec51a4c8a889cc0dddab9a00425b9c32e
yudai2929 <yudai201729@icloud.com>
2023-04-07 22:52:30 +0900

    💄feat: errorのtoasterを変更

---
+++ src/hooks/toast/useErrorToast.ts
@@ -0,0 +1,18 @@
+import { useToast } from '@chakra-ui/react'
+
+export const useErrorToast = () => {
+    const toast = useToast()
+    const toastId = 'error'
+
+    const errorToast = (message: string) => {
+        if (toast.isActive(toastId)) return
+        toast({
+            title: message,
+            status: 'error',
+            isClosable: true,
+            id: toastId,
+            variant: 'subtle',
+        })
+    }
+    return errorToast
+}
