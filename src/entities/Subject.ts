e095396983a117564b7efa6934fad54516835bf4
yudai2929 <yudai201729@icloud.com>
2023-07-04 15:30:32 +0900

    fix: ポルトガルを追加

--- src/entities/Subject.ts
+++ src/entities/Subject.ts
@@ -3,6 +3,7 @@ export const Foreign = {
     france: 'ヨーロッパ学科フランス語圏専攻',
     spanish: 'ヨーロッパ学科スペイン語専攻',
     german: 'ヨーロッパ学科ドイツ語圏専攻',
+    portugal: 'ヨーロッパ学科ポルトガル圏専攻',
     chinese: '中国学科',
     global: '国際関係学科',
 } as const
